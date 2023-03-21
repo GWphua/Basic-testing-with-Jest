479dce5cb82c83f17b524d3a504ce7ba82fa36e8
Guan Wei <phuaguanwei99@gmail.com>
2023-03-21 18:06:23 +0800

    Working with different assertions and matchers.

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return uppercase of valid string", () => {
@@ -11,5 +11,20 @@ describe("Utils test suite", () => {
 
     //assert
     expect(actual).toBe(expected);
+  });
+
+  it("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    // Need to use toEqual for comparing objects.
+    expect(actual.extraInfo).toEqual({});
+    expect(actual.characters).toHaveLength(9);
+    expect(actual.characters).toContain<string>("M");
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+    );
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
