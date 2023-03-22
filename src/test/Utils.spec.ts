7d1e5e3a45aab3e91f7685d60152cf15334348cd
Guan Wei <phuaguanwei99@gmail.com>
2023-03-22 12:05:16 +0800

    3 ways to test for errors.

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -2,17 +2,11 @@ import { getStringInfo, StringUtils, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   describe.only("StringUtils tests", () => {
-    let sut : StringUtils;
-    
+    let sut: StringUtils;
+
     beforeEach(() => {
       sut = new StringUtils();
-      console.log('Setup')
-    })
-
-    afterEach(() => {
-      // clearing mocks
-      console.log('Teardown');
-    })
+    });
 
     it("Should return correct upper case", () => {
       const sut = new StringUtils();
@@ -20,6 +14,28 @@ describe("Utils test suite", () => {
       const actual = sut.toUpperCase("abc");
 
       expect(actual).toBe("ABC");
+    });
+    it("Should throw error on invalid argument - arrow function", () => {
+      function expectError() {
+        const actual = sut.toUpperCase("");
+      }
+
+      expect(() => {
+        sut.toUpperCase("");
+      }).toThrow();
+      expect(() => {
+        sut.toUpperCase("");
+      }).toThrowError("Invalid argument!");
+    });
+    it("Should throw error on invalid argument - try catch block", (done) => {
+      try {
+        sut.toUpperCase("");
+        done("GetStringInfo should throw error for invalid argument!");
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty("message", "Invalid argument!");
+        done();
+      }
     });
   });
 
