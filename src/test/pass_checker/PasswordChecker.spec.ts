79c1d8654e865d27c8b86a59b42bca30beb1df24
Guan Wei <phuaguanwei99@gmail.com>
2023-03-22 15:14:05 +0800

    Password Checker 1

--- src/test/pass_checker/PasswordChecker.spec.ts
+++ src/test/pass_checker/PasswordChecker.spec.ts
@@ -7,7 +7,33 @@ describe("PasswordChecker test suite", () => {
     sut = new PasswordChecker();
   });
 
-  it("Should do nothing for the moment", () => {
-    sut.checkPassword();
+  it("Password with less than 8 chars is invalid", () => {
+    const actual = sut.checkPassword("12345aB");
+    expect(actual).toBe(false);
+  });
+
+  it("Password with less than 8 chars is invalid", () => {
+    const actual = sut.checkPassword("12345678Aa");
+    expect(actual).toBe(true);
+  });
+
+  it("Password with no upper case is invalid", () => {
+    const actual = sut.checkPassword("1234abcd");
+    expect(actual).toBe(false);
+  });
+
+  it("Password with upper case letter is valid", () => {
+    const actual = sut.checkPassword("1234ASD1b23");
+    expect(actual).toBe(true);
+  });
+
+  it("Password with no lower case is invalid", () => {
+    const actual = sut.checkPassword("1234ABCD");
+    expect(actual).toBe(false);
+  });
+
+  it("Password with upper case letter is valid", () => {
+    const actual = sut.checkPassword("1234ASDb123");
+    expect(actual).toBe(true);
   });
 });
