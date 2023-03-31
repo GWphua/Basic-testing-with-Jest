ae928ccdc020cc0d84c410ef0e254199dba3235d
Guan Wei <phuaguanwei99@gmail.com>
2023-03-31 16:50:36 +0800

    Set up DataBase mock.

--- src/test/server_app/data/SessionTokenDataAccess.test.ts
+++ src/test/server_app/data/SessionTokenDataAccess.test.ts
@@ -1,3 +1,31 @@
+import { DataBase } from "../../../app/server_app/data/DataBase";
+import { SessionTokenDataAccess } from "../../../app/server_app/data/SessionTokenDataAccess";
+
+const insertMock = jest.fn();
+const updateMock = jest.fn();
+const getByMock = jest.fn();
+
+jest.mock("../../../app/server_app/data/DataBase", () => {
+  return {
+    DataBase: jest.fn().mockImplementation(() => {
+      return {
+        insert: insertMock,
+        update: updateMock,
+        getBy: getByMock,
+      };
+    }),
+  };
+});
+
 describe("SessionTokenDataAccess test suite", () => {
-  
-})
+  let sut: SessionTokenDataAccess;
+
+  beforeEach(() => {
+    sut = new SessionTokenDataAccess();
+    expect(DataBase).toBeCalledTimes(1);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
