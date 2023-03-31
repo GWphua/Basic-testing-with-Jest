3723b29461417df7a5016782bb93ffbe9e116280
Guan Wei <phuaguanwei99@gmail.com>
2023-03-31 15:55:26 +0800

    Mocking consumer classes.

--- src/test/server_app/data/UserCredentialsDataAccess.test.ts
+++ src/test/server_app/data/UserCredentialsDataAccess.test.ts
@@ -1,5 +1,6 @@
 import { DataBase } from "../../../app/server_app/data/DataBase";
 import { UserCredentialsDataAccess } from "../../../app/server_app/data/UserCredentialsDataAccess";
+import { Account } from "../../../app/server_app/model/AuthModel";
 
 const insertMock = jest.fn();
 const getByMock = jest.fn();
@@ -18,6 +19,14 @@ jest.mock("../../../app/server_app/data/DataBase", () => {
 describe("UserCredentialsDataAccess test suite", () => {
   let sut: UserCredentialsDataAccess;
 
+  const someAccount: Account = {
+    id: "",
+    password: "somePassword",
+    userName: "someUserName",
+  };
+
+  const someId = "1234";
+
   beforeEach(() => {
     sut = new UserCredentialsDataAccess();
     expect(DataBase).toHaveBeenCalledTimes(1);
@@ -28,4 +37,31 @@ describe("UserCredentialsDataAccess test suite", () => {
   });
 
   it("should call mock db", () => {});
+
+  it("should add user and return the id", async () => {
+    insertMock.mockResolvedValueOnce(someId);
+
+    const actualId = await sut.addUser(someAccount);
+
+    expect(actualId).toBe(someId);
+    expect(insertMock).toHaveBeenCalledWith(someAccount);
+  });
+
+  it("should add user and return the id", async () => {
+    getByMock.mockResolvedValueOnce(someAccount);
+
+    const actualUser = await sut.getUserById(someId);
+
+    expect(actualUser).toBe(someAccount);
+    expect(getByMock).toHaveBeenCalledWith("id", someId);
+  });
+
+  it("should get user by name", async () => {
+    getByMock.mockResolvedValueOnce(someAccount);
+
+    const actualUser = await sut.getUserByUserName(someAccount.userName);
+
+    expect(actualUser).toEqual(someAccount);
+    expect(getByMock).toHaveBeenCalledWith("userName", someAccount.userName);
+  });
 });
