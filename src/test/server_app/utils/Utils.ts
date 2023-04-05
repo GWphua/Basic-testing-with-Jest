66cb388c3c5b2738775e6f696ad337cabbf625d4
Guan Wei <phuaguanwei99@gmail.com>
2023-04-05 18:00:14 +0800

    Create Utils.ts

---
+++ src/test/server_app/utils/Utils.ts
@@ -0,0 +1,15 @@
+const requestMock = {
+  on: jest.fn(),
+};
+
+const someObject = {
+  name: "John",
+  age: 30,
+  city: "Paris",
+};
+
+const someObjectAsString = JSON.stringify(someObject);
+
+describe("getRequestBody test suite", () => {
+  //TODO
+});
